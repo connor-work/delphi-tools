1bac24582721a239d57da2286cd10ab084856985
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-08-28 18:08:35 +0200

    Added minimal implementation of Delphi Source Code Writer that only produces empty units, including known-answer test

---
+++ code-writer.tests/KnownAnswerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Work.Connor.Delphi.CodeWriter.Tests
+{
+    public class KnownAnswerTest
+    {
+        [Theory]
+        [MemberData(nameof(KnownUnitSourceCode))]
+        public void ProducesExpectedUnitSourceCode(Unit unit, string expectedCode) => Assert.Equal(expectedCode, unit.ToSourceCode());
+
+        public static TheoryData<Unit, string> KnownUnitSourceCode()
+        {
+            TheoryData<Unit, string> data = new TheoryData<Unit, string>();
+            data.Add(new Unit()
+            {
+                Heading = new UnitIdentifier()
+                {
+                    Unit = "UnitX",
+                    Namespace = { "Space1", "Space2" }
+                }
+            },
+@"unit Space1.Space2.UnitX;
+
+end.
+"
+            );
+            return data;
+        }
+    }
+}

7303b42e8a1a4948423a3e7638637bb13ec7f467
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-04 10:15:24 +0200

    Implement schema and test for procedure parameters

---
+++ code-writer.tests/known-answer-test-vectors/parameters.pas
@@ -0,0 +1,14 @@
+unit UnitX;
+
+interface
+
+type
+  ClassX = class
+  procedure ProcedureX(ParamX: Integer);
+
+  procedure ProcedureY(ParamY1: Integer, ParamY2: array of Integer);
+  end;
+
+implementation
+
+end.

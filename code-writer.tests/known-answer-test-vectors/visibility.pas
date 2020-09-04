c23ea8a704c6176b6edd3c22278cf6cb311373e5
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-04 10:54:47 +0200

    QV5-34 Add class member visibility attribute to schema and tests

---
+++ code-writer.tests/known-answer-test-vectors/visibility.pas
@@ -0,0 +1,18 @@
+unit UnitX;
+
+interface
+
+type
+  ClassX = class
+  procedure ProcedureX;
+
+  public procedure ProcedureY;
+
+  protected procedure ProcedureZ;
+
+  private procedure ProcedureA;
+  end;
+
+implementation
+
+end.

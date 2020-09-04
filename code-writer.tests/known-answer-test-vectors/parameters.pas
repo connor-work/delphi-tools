78b4ffe9e1c10f77befcdb42e24fff6d9d9abde2
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-04 10:23:03 +0200

    QV5-34 Correct Delphi syntax in parameter test

--- code-writer.tests/known-answer-test-vectors/parameters.pas
+++ code-writer.tests/known-answer-test-vectors/parameters.pas
@@ -6,7 +6,7 @@ type
   ClassX = class
   procedure ProcedureX(ParamX: Integer);
 
-  procedure ProcedureY(ParamY1: Integer, ParamY2: array of Integer);
+  procedure ProcedureY(ParamY1: Integer; ParamY2: array of Integer);
   end;
 
 implementation

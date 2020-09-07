98ee0ce8b4c007b8c7046970f4ba52a59bfb19d7
Connor Röhricht (Sotax AG) <connor.roehricht@sotax.com>
2020-09-07 07:52:19 +0200

    QV5-34 Add annotation comments to schema + tests

--- code-writer.tests/known-answer-test-vectors/emptyUnit.pas
+++ code-writer.tests/known-answer-test-vectors/emptyUnit.pas
@@ -1,3 +1,6 @@
+/// <summary>
+/// This is an empty unit used for testing.
+/// </summary>
 unit Space1.Space2.UnitX;
 
 interface
--- code-writer.tests/known-answer-test-vectors/fields.pas
+++ code-writer.tests/known-answer-test-vectors/fields.pas
@@ -6,6 +6,9 @@ type
   ClassX = class
   var FieldX: Integer;
 
+  /// <summary>
+  /// This is a field used for testing.
+  /// </summary>
   var FieldY: string;
   end;
 

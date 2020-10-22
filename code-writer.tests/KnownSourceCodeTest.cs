54f33ace62d4cef7a960a075d8c94ff491997f80
Connor Röhricht (connor.work) <connor@connor.work>
2020-10-22 06:23:53 +0200

    Revert "Debugging bug that appears only on Travis CI with temporary debug exception"
    This reverts commit c092bec36fa2638ca102a2c681d875dda9197d1e.

--- code-writer.tests/KnownSourceCodeTest.cs
+++ code-writer.tests/KnownSourceCodeTest.cs
@@ -14,7 +14,6 @@
 /// limitations under the License.
 
 using Google.Protobuf;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -163,7 +162,6 @@ namespace Work.Connor.Delphi.CodeWriter.Tests
                 get
                 {
                     string resourceName = $"{name}.{protobufJsonFileExtension}";
-                    throw new Exception(string.Join(", ", allProgramMessageResources.GetIDs()));
                     using StreamReader reader = new StreamReader(allProgramMessageResources.GetResourceStream(resourceName) ?? throw new FileNotFoundException(resourceName));
                     return jsonParser.Parse<Program>(reader);
                 }

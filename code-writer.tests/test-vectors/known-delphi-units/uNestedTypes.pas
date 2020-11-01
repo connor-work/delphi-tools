889a6b436e2621f3b68cf44b183a22bb946a3b5a
Connor Röhricht (connor.work) <connor@connor.work>
2020-11-01 20:51:20 +0100

    #14 Corrected indentation level within class declaration for uNestedTypes test

--- code-writer.tests/test-vectors/known-delphi-units/uNestedTypes.pas
+++ code-writer.tests/test-vectors/known-delphi-units/uNestedTypes.pas
@@ -8,20 +8,20 @@ interface
 
 type
   ClassX = class
-  type
-    /// <summary>
-    /// This is an empty nested class used for testing.
-    /// </summary>
-    ClassY = class
-    end;
+    type
+      /// <summary>
+      /// This is an empty nested class used for testing.
+      /// </summary>
+      ClassY = class
+      end;
 
-  private type
-    /// <summary>
-    /// This is a nested enumerated type used for testing.
-    /// </summary>
-    EnumX = (
-      exValueX
-    );
+    private type
+      /// <summary>
+      /// This is a nested enumerated type used for testing.
+      /// </summary>
+      EnumX = (
+        exValueX
+      );
   end;
 
 implementation

24d07662e80ecd3c09182d770fbe74e0f178ff86
Connor Röhricht (connor.work) <connor@connor.work>
2020-11-02 08:03:08 +0100

    #16 Implemented uses clause sorting

---
+++ code-writer.tests/DelphiSourceCodeUtilityTest.cs
@@ -0,0 +1,46 @@
+﻿/// Copyright 2020 Connor Roehricht (connor.work)
+/// Copyright 2020 Sotax AG
+/// 
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+/// 
+///     http://www.apache.org/licenses/LICENSE-2.0
+/// 
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+using Google.Protobuf.Collections;
+using Xunit;
+
+namespace Work.Connor.Delphi.CodeWriter.Tests
+{
+    /// <summary>
+    /// Tests Delphi source code utility code.
+    /// </summary>
+    public class DelphiSourceCodeUtilityTest
+    {
+        /// <summary>
+        /// <see cref="Delphi.SourceCodeExtensions.SortUsesClause(RepeatedField{UnitReference})"/> sorts a uses clause correctly.
+        /// </summary>
+        [Fact]
+        public void SortsUsesClauseCorrectly()
+        {
+            RepeatedField<UnitReference> unsortedClause = new RepeatedField<UnitReference>
+            {
+                new UnitReference() { Unit = new UnitIdentifier() { Unit = "SysUtils", Namespace = { "System" } } },
+                new UnitReference() { Unit = new UnitIdentifier() { Unit = "Collections", Namespace = { "System", "Generics" } } },
+                new UnitReference() { Unit = new UnitIdentifier() { Unit = "System" } }
+            };
+            RepeatedField<UnitReference> sortedClause = new RepeatedField<UnitReference>
+            {
+                unsortedClause[2], unsortedClause[1], unsortedClause[0]
+            };
+            unsortedClause.SortUsesClause();
+            Assert.Equal(sortedClause, unsortedClause);
+        }
+    }
+}
---
+++ code-writer/DelphiSourceCodeUtility.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Protobuf.Collections;
+
+namespace Work.Connor.Delphi
+{
+    /// <summary>
+    /// Comparer that sorts sequences of comparables lexicographically.
+    /// </summary>
+    /// <typeparam name="T">The type of enumerable elements</typeparam>
+    internal class EnumerableComparer<T> : Comparer<IEnumerable<T>?> where T : IComparable<T>
+    {
+        public override int Compare(IEnumerable<T>? x, IEnumerable<T>? y)
+        {
+            if (x is null) return y is null ? 0 : -1;
+            if (y is null) return 1;
+            foreach ((T leftElement, T rightElement) in x.Zip(y))
+            {
+                int elementCompare = leftElement.CompareTo(rightElement);
+                if (elementCompare != 0) return elementCompare;
+            }
+            return x.Count().CompareTo(y.Count());
+        }
+    }
+
+    public sealed partial class UnitReference : IComparable<UnitReference?>
+    {
+        /// <summary>
+        /// Lexicographical comparer for string sequences
+        /// </summary>
+        private static EnumerableComparer<string> stringSequenceComparer = new EnumerableComparer<string>();
+
+        // Implement lexicographical comparison of unit references
+
+        public int CompareTo(UnitReference? other)
+        {
+            if (other == null) return 1;
+            return stringSequenceComparer.Compare(Unit.Namespace.Append(Unit.Unit),
+                                                  other.Unit.Namespace.Append(other.Unit.Unit));
+        }
+
+        public static bool operator ==(UnitReference? left, UnitReference? right) => (left is null) ? (right is null)
+                                                                                                    : left.Equals(right);
+
+        public static bool operator >(UnitReference? left, UnitReference? right) => (left is object) && left.CompareTo(right) > 0;
+
+        public static bool operator <(UnitReference? left, UnitReference? right) => (left is null) ? (right is object)
+                                                                                                   : left.CompareTo(right) > 0;
+
+        public static bool operator >=(UnitReference? left, UnitReference? right) => (left == right) || (left < right);
+
+        public static bool operator <=(UnitReference? left, UnitReference? right) => (left == right) || (left > right);
+
+        public static bool operator !=(UnitReference? left, UnitReference? right) => !(left == right);
+    }
+
+    /// <summary>
+    /// Extensions to Delphi source code types for convenient source code manipulation.
+    /// </summary>
+    public static partial class SourceCodeExtensions
+    {
+        /// <summary>
+        /// Sorts a Delphi uses clause in-place.
+        /// </summary>
+        /// <param name="usesClause">The uses clause to sort</param>
+        public static void SortUsesClause(this RepeatedField<UnitReference> usesClause)
+        {
+            RepeatedField<UnitReference> oldClause = usesClause.Clone();
+            usesClause.Clear();
+            usesClause.AddRange(oldClause.OrderBy(reference => reference));
+        }
+    }
+}
